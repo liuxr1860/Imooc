f593de78a4ed7bae11a33a3d22276732831a4e0f
liuxr1860 <liuxr1861@163.com>
2018-06-03 23:35:52 +0800

    第二章，对象的创建和使用

---
+++ pc_project/src/com/imooc/InitalTelphone.java
@@ -0,0 +1,14 @@
+package com.imooc;
+
+public class InitalTelphone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Telphone phone = new Telphone();
+		phone.call();
+		phone.sendMessage();
+		
+
+	}
+
+}
--- pc_project/src/com/imooc/Telphone.java
+++ pc_project/src/com/imooc/Telphone.java
@@ -1,5 +1,20 @@
 package com.imooc;
 
 public class Telphone {
+	//构造函数
+	Telphone() {
+		System.out.println("构造函数执行");
+	}
+	//属性
+	float screen = 4.0f;
+	float cpu = 1.0f;
+	float mem = 2.0f;
+	//方法
+	void call( ) {
+		System.out.println("Telphone具有打电话的功能");
+	}
+    void sendMessage( ) {
+    	System.out.println("Telphone具有发短信的功能");
+    }
 
 }
