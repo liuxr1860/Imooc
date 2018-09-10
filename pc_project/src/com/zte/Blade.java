c4cbc84c884a36a5b4fd8494adb5df14cb7ab854
liuxr1860 <liuxr1861@163.com>
2018-09-10 21:32:49 +0800

    this 在构造函数中的使用

--- pc_project/src/com/zte/Blade.java
+++ pc_project/src/com/zte/Blade.java
@@ -1,5 +1,32 @@
 package com.zte;
 
 public class Blade {
+	/*构造函数*/
+	Blade(){
+		System.out.println("无参的构造方法被调用");
+	}
+	Blade(String DeviceName,String CpuManufactory){
+		System.out.println("有参的构造方法被调用");
+		this.DeviceName = DeviceName;
+		this.CpuManufactory = CpuManufactory;
+	}
+	/*属性*/
+	String DeviceName="6739Device";
+	String CpuManufactory = "MTK";
+	int DPI = 320;
+	float ROM = 32.0f;
+	float RAM = 2.0f;
+	/*方法*/
+	void getDeviceName() {
+		System.out.println("DeviceName is:"+DeviceName);
+	}
+	void getCpuInfo() {
+		System.out.println("CPU Manufactory is:"+CpuManufactory);		
+	}
+	float getDataSize(float systemSize) {
+		/*成员变量*/
+		float dataSize = this.ROM - systemSize;
+		return dataSize;
+	}
 
 }
--- pc_project/src/com/zte/InitialBlade.java
+++ pc_project/src/com/zte/InitialBlade.java
@@ -4,6 +4,11 @@ public class InitialBlade {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Blade A530 = new Blade();
+		Blade A531 = new Blade("A531","MTK");
+		A530.getDeviceName();
+		A531.getDeviceName();
+		A531.getCpuInfo();
 
 	}
 
