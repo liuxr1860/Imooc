ccee9e2f22d8908cdf9c86a808e3025a7fd2af62
liuxr1860 <liuxr1861@163.com>
2018-09-11 21:27:23 +0800

    方法的多态

--- pc_project/src/com/zte/InitialBlade.java
+++ pc_project/src/com/zte/InitialBlade.java
@@ -4,7 +4,7 @@ public class InitialBlade {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Blade A530 = new Blade();
+		Blade A530 = new Blade();
 //		Blade A531 = new Blade("A531","MTK");
 //		A530.getDeviceName();
 //		A531.getDeviceName();
@@ -14,6 +14,7 @@ public class InitialBlade {
 		V9.getCpuInfo();
 		
 		Blade V9vita = new BladeV9();
+		V9vita.getDeviceName();/*方法的多态*/
 
 	}
 
